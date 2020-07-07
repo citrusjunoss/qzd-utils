b5bda8329f56db5fa5725f18f43f3a220bd8dfdb
chengjunchao <chengjunchao@hotmail.com>
2020-07-07 16:13:21 +0800

    feat: export tools queryparse

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,8 @@
-import { filterParams } from './queryParse';
-
+// 参数处理
+export * from './queryParse';
 // 验证模块
 export * from './validate';
 // 加密模块
 export * from './signture';
-console.log(11111111111);
-
-console.log(
-  filterParams({
-    a: 1,
-    b: 0,
-    c: false,
-    d: null,
-    e: void 0,
-    f: '',
-    h: { a: [{ b: 0, c: null }] },
-  }),
-  2222222,
-);
+// 常用工具类 文件名 size处理
+export * from './tool';
