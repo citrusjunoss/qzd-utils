54247f87a10e2b696350be4c221a7172c016ecfb
chengjunchao <chengjunchao@hotmail.com>
2021-03-17 13:45:34 +0800

    fix(validate): isEmptyObj bug

--- lib/env.d.ts
+++ lib/env.d.ts
@@ -1,4 +1,4 @@
-declare type browserType = 'trident' | 'presto' | 'webKit' | 'gecko' | 'mobile' | 'ios' | 'android' | 'iPhone' | 'iPad' | 'webApp' | 'QQbrw' | 'weiXin' | 'QQ' | 'weiBo' | 'ucLowEnd' | 'ucweb' | 'ucSpecial' | 'webview' | 'Symbian' | 'ucSB' | 'qzd' | 'huaWei';
+declare type browserType = 'trident' | 'presto' | 'webKit' | 'gecko' | 'mobile' | 'ios' | 'android' | 'iPhone' | 'iPad' | 'webApp' | 'QQbrw' | 'weiXin' | 'wxApp' | 'QQ' | 'weiBo' | 'ucLowEnd' | 'ucweb' | 'ucSpecial' | 'webview' | 'Symbian' | 'ucSB' | 'qzd' | 'huaWei';
 /**
  * @description 判断是否是在某个环境内运行 支持数组 和 字符串 目前支持类型有
  * @param  | 'trident' | 'presto' | 'webKit' | 'gecko' | 'mobile' | 'ios' | 'android' | 'iPhone' | 'iPad'| 'webApp'
